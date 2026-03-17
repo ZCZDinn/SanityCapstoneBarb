61a8363ffb1a7eac7a2821dd1c0148b2d8af8717
Eric Stock <ericstockteacher@gmail.com>
2026-03-17 09:55:47 -0230

    -added image type to portable text schema for greetingType. -added newsItemType schema to show example of fixed schema

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,3 +1,4 @@
 import {greetingType} from './greetingType'
+import {newsItemType} from './newsItemType'
 
-export const schemaTypes = [greetingType]
+export const schemaTypes = [greetingType, newsItemType]
