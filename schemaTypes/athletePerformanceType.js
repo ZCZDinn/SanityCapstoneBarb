1d8b1de2140bc82ffdba6d99541b72d78ccaa3c3
Madison <madison.hartery59@ed.cna.nl.ca>
2026-04-05 20:38:43 -0230

    contact switched to sanity, athlete performance almost switched to sanity

--- schemaTypes/athletePerformanceType.js
+++ schemaTypes/athletePerformanceType.js
@@ -6,10 +6,125 @@ export const athletePerformanceType = defineType({
   type: 'document',
   fields: [
     {
-        title: 'Content', 
-        name: 'content',
-        type: 'array', 
-        of: [{type: 'block'}, {type:'image'}]
-    }
+      name: 'mainHeader',
+      title: 'The Main Header of the athlete performance Page',
+      type: 'string'
+    },
+    {
+      name: 'athletePerformanceDescription',
+      title: 'athlete Performance Description',
+      type: 'array',
+      of: [{type: 'block'}],
+      description: 'Introduction to athelete performance program'
+    },
+    {
+      name: 'cyclingActivitySubheading',
+      title: 'Cycling Activity Subheading',
+      type: 'string'
+    },
+    {
+      name: 'cyclingActivityDescription',
+      title: 'Cycling Activity Description',
+      type: 'array',
+      of: [{type: 'block'}],
+      description: 'Introduction to the cycling activity table below'
+    },
+    {
+      name: 'cyclingActivityTable',
+      title: 'cycling Activity Table',
+      type: 'table',
+      description: 'Table with info on columns: Cycling activity, and Description'
+    }, {
+      name: 'contact',
+      title: 'Contact Info',
+      type: 'array',
+      of: [{type: 'block'}],
+      description: 'general contact info for interest in cycling'
+    },
+    {
+      name: 'highPerformanceSubheading',
+      title: 'High Performance Subheading',
+      type: 'string'
+    },
+    {
+      name: 'highPerformanceDescription',
+      title: 'High Performance Description',
+      type: 'array',
+      of: [{type: 'block'}],
+      description: 'Paragraph describing high performance program'
+    },
+    {
+      name: 'roadHighPerformanceSubheading',
+      title: 'Road High Performance Subheading',
+      type: 'string'
+    },
+    {
+      name: 'roadHighPerformanceAddress',
+      title: 'Road High Performance Address',
+      type: 'text',
+      description: 'email address for road coach'
+    },
+    {
+      name: 'mountainHighPerformanceSubheading',
+      title: 'Mountain High Performance Subheading',
+      type: 'string'
+    },
+    {
+      name: 'mountainHighPerformanceAddress',
+      title: 'Mountain High Performance Address',
+      type: 'text',
+      description: 'email address for mountain biking coach'
+    },
+        {
+      name: 'canadaGamesSubheading',
+      title: 'Canada Games Subheading',
+      type: 'string'
+    },
+    {
+      name: 'canadaGamesDescription',
+      title: 'Canada Games Description',
+      type: 'array',
+      of: [{type: 'block'}],
+      description: 'Info on Canada Games'
+    },
+    {
+      name: 'teamsTable',
+      title: 'Team MembersTable',
+      type: 'table',
+      description: 'Table with members of both teams, road and mountain bike'
+    },
+    {
+      name: 'teamLocationDescription',
+      title: 'Team Location Description',
+      type: 'array',
+      of: [{type: 'block'}],
+      description: 'List where each team competes'
+    },
+    {
+      name: 'roadTeamImage',
+      title: 'road Team Image',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: 'mountainTeamImage',
+      title: 'Mountain Team Image',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: 'teamExpectationsSubheading',
+      title: 'Team Expectations Subheading',
+      type: 'string'
+    },
+    {
+      name: 'teamExpectations',
+      title: 'Team Expectations',
+      type: 'text',
+      description: 'Description of team expectations for athletes'}
     ],
 })
--- schemaTypes/contactInfoType.js
+++ schemaTypes/contactInfoType.js
@@ -6,10 +6,66 @@ export const contactInfoType = defineType({
   type: 'document',
   fields: [
     {
-        title: 'Content', 
-        name: 'content',
-        type: 'array', 
-        of: [{type: 'block'}, {type:'image'}]
+      name: 'mainHeader',
+      title: 'The Main Header of the Contact Page',
+      type: 'string'
+    },
+    {
+      name: 'mailingAddressHeading',
+      title: 'Mailing Address Subheading',
+      type: 'string',
+      initialValue: 'Mailing Address'
+    },
+    {
+      name: 'mailingAddress',
+      title: 'Address',
+      type: 'text',
+      description: 'The mailing address paragraph'
+    },
+    {
+      name: 'generalInquiriesHeading',
+      title: 'General Inquires Subheading',
+      type: 'string',
+      initialValue: 'General Inquires'
+    },
+    {
+      name: 'generalInquiriesEmail',
+      title: 'Email Address for General Inquires',
+      type: 'email'
+    },
+    {
+      name: 'presidentHeading',
+      title: 'President Subheading',
+      type: 'string',
+      initialValue: 'President'
+    },
+    {
+      name: 'presidentEmail',
+      title: 'Email Address for President',
+      type: 'email'
+    },
+    {
+      name: 'boardOfDirectorsHeading',
+      title: 'Board of Directors Subheading',
+      type: 'string'
+    },
+    {
+      name: 'boardOfDirectorsDescription',
+      title: 'Description',
+      type: 'text',
+      description: 'Paragraph describing the board of directors'
+    },
+    {
+      name: 'currentBoardHeading',
+      title: 'Current board Subheading',
+      type: 'string',
+      description: 'Heading above current board table'
+    },
+    {
+      name: 'currentBoardTable',
+      title: 'Board Members Table',
+      type: 'table',
+      description: 'Table with columns: Position and Name'
     }
-    ],
+  ]
 })
